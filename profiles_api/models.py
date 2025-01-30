9d3cd08b0c391b033c863d3e0ec7d93b8daeec08
aliahadie <ali.ahadi.official@gmail.com>
2025-01-30 18:02:43 +0330

    create class base view with serializer

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,19 +3,20 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUse
 
 class UserProfileManager(BaseUserManager):
     def create_user(self,email,name,password=None):
+
         if not email:
-            raise ValueError('User must have email address')
+            raise ValueError('Email Must have enterd')
         email=self.normalize_email(email)
         user=self.model(email=email,name=name)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user    
     
     def create_superuser(self,email,name,password):
-        user=self.create_superuser(email,name,password)
+        user=self.create_user(email,name,password)
         user.is_superuser=True
         user.is_staff=True
-        user.save(using=self.__db)
+        user.save()
         return user
 
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api.serializers import HelloSerializer
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    serializer_class=HelloSerializer
+
+    def get(self,request):
+        an_api=[
+            'hello this is a test from api',
+            'with purpose of creating rest api app'
+        ]
+        message='message'
+        context={'an_api':an_api,'message':message}
+        return Response(context)
+    
+
+    def post(self,request):
+
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            email=serializer.validated_data.get('email')
+            message=f'Hello {name} your email is {email}'
+            return Response({'message':message})
+         
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk=None):
+        return Response({'method':'PUT'})
+    
+    def patch(self,request,pk=None):
+        return Response({'method':'PATCH'}) 
+    
+    def delete(self,request,pk=None):
+        return Response({'method':'DELETE'})
